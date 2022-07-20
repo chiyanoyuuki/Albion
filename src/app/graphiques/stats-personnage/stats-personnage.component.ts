5626af75e0dc7433f354de9be8d207496b315554
chiyanoyuuki <charles.poure@hotmail.com>
2022-07-20 23:28:24 +0200

    pas mal de trucs 2

--- src/app/graphiques/stats-personnage/stats-personnage.component.ts
+++ src/app/graphiques/stats-personnage/stats-personnage.component.ts
@@ -1,5 +1,6 @@
+import { CdkDragEnd, CdkDragStart } from '@angular/cdk/drag-drop';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { Data, Entite, ObjetInventaire, Quete } from 'src/app/model';
+import { Data, Entite, Equipement, ObjetInventaire, Quete } from 'src/app/model';
 
 @Component({
   selector: 'app-stats-personnage',
@@ -15,8 +16,13 @@ export class StatsPersonnageComponent implements OnInit {
   public gain: string;
   public formulaire: string;
   public ongletActif: string = "inventaire";
-  public objetActif: ObjetInventaire | undefined;
+  public objetActifInventaire: ObjetInventaire | undefined;
+  public objetActifStuff: Equipement | undefined;
+  public sortActif: { nom: string, image: string } | undefined;
   public quantite: string;
+  public focus: boolean = false;
+  public emplacementFocused: string;
+  public itemDragged: string;
 
   constructor() { }
 
@@ -27,28 +33,49 @@ export class StatsPersonnageComponent implements OnInit {
     if (this.gain != clicked) {
       this.gain = clicked;
     } else if (this.gain == clicked) {
+      if (this.gain == "") { return }
       if (clicked == 'Niveau') {
         perso.niveau += 1;
       }
-      perso.stats.forEach(element => {
-        if (element.nom == clicked) {
-          element.qte += 1;
+      let statCliquee = perso.stats.find((stat: { nom: string, qte: number }) => stat.nom == clicked);
+      if (statCliquee) {
+        statCliquee.qte += 1;
+      }
+      let objetClique = perso.inventaire.find((objet: ObjetInventaire) => objet.nom == clicked);
+      if (objetClique) {
+        objetClique.qte -= 1;
+        if (objetClique.qte == 0) {
+          perso.inventaire.splice(perso.inventaire.indexOf(objetClique), 1);
+          perso.inventaire.push({ "emplacement": '', "nom": '', "image": '', qte: 0 });
         }
-      });
-      perso.inventaire.forEach(element => {
-        if (element.nom == clicked) {
-          element.qte -= 1;
-          let item = perso.inventaire.find(objet => objet.nom == clicked);
-          if (element.qte == 0) {
-            if (item) {
-              perso.inventaire.splice(perso.inventaire.indexOf(item), 1);
-              perso.inventaire.push({ "nom": '', "image": '', qte: 0 });
-            }
-          } else if (item && element.qte < 0) {
-            item.qte = 0;
-          }
+      }
+      this.gain = "";
+    }
+  }
+
+  clickGainStuff(perso: Entite, clicked: Equipement) {
+    if (this.gain != clicked.objet.nom) {
+      this.gain = clicked.objet.nom;
+    } else if (this.gain == clicked.objet.nom) {
+      if (this.gain == "") { return; }
+      let objetDejaDansInventaire = this.perso.inventaire.find((objet: ObjetInventaire) => objet.nom == clicked.objet.nom);
+      let emplacementLibre = this.perso.inventaire.find((objet: ObjetInventaire) => objet.nom == "");
+      if (!objetDejaDansInventaire && !emplacementLibre) { return; }
+
+      if (objetDejaDansInventaire) {
+        objetDejaDansInventaire.qte += 1;
+      }
+      else {
+        if (emplacementLibre) {
+          emplacementLibre.emplacement = clicked.objet.emplacement;
+          emplacementLibre.image = clicked.objet.image;
+          emplacementLibre.nom = clicked.objet.nom;
+          emplacementLibre.qte = 1;
+
+          console.log(emplacementLibre);
         }
-      });
+      }
+      clicked.objet = { "emplacement": '', "nom": '', "image": '', qte: 0 };
       this.gain = "";
     }
   }
@@ -61,5 +88,73 @@ export class StatsPersonnageComponent implements OnInit {
 
   close() {
     this.closeForm.emit();
+  }
+
+  getLeft(emplacement: Equipement) {
+    let tmp = this.data.positionsStuff.find((position: { emplacement: string, x: number, y: number }) => position.emplacement == emplacement.emplacement);
+    return tmp?.x + "px";
+  }
+
+  getTop(emplacement: Equipement) {
+    let tmp = this.data.positionsStuff.find((position: { emplacement: string, x: number, y: number }) => position.emplacement == emplacement.emplacement);
+    return tmp?.y + "px";
+  }
+
+  public dragEnd($event: CdkDragEnd, item: ObjetInventaire) {
+    let tmp = $event.source.getFreeDragPosition();
+
+    const offsetsStart = document.getElementById(this.itemDragged)?.getBoundingClientRect();
+    const offsetsEnd = document.getElementById(this.emplacementFocused)?.getBoundingClientRect();
+    if (offsetsStart && offsetsEnd) {
+      const endLeft = offsetsStart.left + tmp.x + 20;
+      const endTop = offsetsStart.top + tmp.y + 20;
+
+      const posStartX = offsetsEnd.left;
+      const posStartY = offsetsEnd.top;
+
+      if (endLeft >= posStartX && endLeft <= posStartX + 50 && endTop >= posStartY && endTop <= posStartY + 50) {
+        let emplacementVide = this.perso.inventaire.find((objet: ObjetInventaire) => objet.nom == "");
+        let stuffConcerne = this.perso.stuff.find((stuff: Equipement) => stuff.emplacement == this.emplacementFocused);
+        let ancienStuff = Object.assign({}, stuffConcerne);
+
+        if ((emplacementVide || (!emplacementVide && item.qte == 1)) && ancienStuff.objet.nom != item.nom) {
+          if (stuffConcerne) {
+            stuffConcerne.objet = item;
+            let objetDansInventaire = this.perso.inventaire.find((objet: ObjetInventaire) => objet.nom == item.nom);
+            if (objetDansInventaire) {
+              objetDansInventaire.qte -= 1;
+              if (objetDansInventaire.qte == 0) {
+                this.perso.inventaire.splice(this.perso.inventaire.indexOf(objetDansInventaire), 1);
+                this.perso.inventaire.push({ "emplacement": '', "nom": '', "image": '', qte: 0 });
+              }
+            }
+
+            if (ancienStuff.objet.nom != "") {
+              let objetDejaDansInventaire = this.perso.inventaire.find((objet: ObjetInventaire) => objet.nom == ancienStuff.objet.nom);
+              if (objetDejaDansInventaire) {
+                objetDejaDansInventaire.qte += 1;
+              }
+              else {
+
+                let emplacementLibre = this.perso.inventaire.find((objet: ObjetInventaire) => objet.nom == "");
+                if (emplacementLibre) {
+                  emplacementLibre.emplacement = ancienStuff.objet.emplacement;
+                  emplacementLibre.image = ancienStuff.objet.image;
+                  emplacementLibre.nom = ancienStuff.objet.nom;
+                  emplacementLibre.qte = 1;
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    $event.source._dragRef.reset();
+    this.emplacementFocused = "";
+  }
+
+  public dragStart(item: ObjetInventaire) {
+    this.emplacementFocused = item.emplacement;
+    this.itemDragged = item.nom;
   }
 }
